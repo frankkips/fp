0677c6dff397654e649d216bae1a5f5f87ba457d
frankkips <franklinekiplagat1@gmail.com>
2025-02-25 17:19:47 +0300

    changed origin

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://potato-det.vercel.app",
         methods: ["GET","POST"],
         credentials: true
     }
